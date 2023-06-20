94aa514d54b8ec73c07e9ce5676b3d4f2c6ee160
Underemployed <underemployed871@gmail.com>
2023-06-20 11:03:42 +0530

    Completed

--- script.js
+++ script.js
@@ -1,121 +1,165 @@
-  const solveBtn = document.getElementById('solveBtn');
-  const board = document.getElementById('board');
-  let cells = [];
+const solveBtn = document.getElementById('solveBtn');
+const resetBtn = document.getElementById('resetBtn');
+const board = document.getElementById('board');
+let cells = [];
 
-  // Generate the Sudoku board with input cells
+// Generate the Sudoku board with input cells
+for (let i = 0; i < 9; i++) {
+  for (let j = 0; j < 9; j++) {
+    const input = document.createElement('input');
+    input.type = 'text';
+    input.className = 'cell';
+    input.maxLength = '1';
+    board.appendChild(input);
+    cells.push(input);
+  }
+}
+
+// Event listener for the solve button
+solveBtn.addEventListener('click', solveSudoku);
+
+// Event listener for the reset button
+resetBtn.addEventListener('click', () => {
+  location.reload();
+});
+
+function solveSudoku() {
+  const puzzle = [];
+  let index = 0;
+
+  // Create a 2D array representing the Sudoku puzzle
+  for (let i = 0; i < 9; i++) {
+    const row = [];
+    for (let j = 0; j < 9; j++) {
+      const value = parseInt(cells[index].value) || 0;
+      row.push(value);
+      index++;
+    }
+    puzzle.push(row);
+  }
+
+  // Solve the puzzle
+  const solution = solveSudokuHelper(puzzle);
+
+  // Update the board with the solved puzzle
+  animateSolution(solution);
+}
+
+function solveSudokuHelper(board) {
+  if (!findEmptyCell(board)) {
+    return board;
+  }
+
+  const [row, col] = findEmptyCell(board);
+
+  for (let num = 1; num <= 9; num++) {
+    if (isValidMove(board, row, col, num)) {
+      board[row][col] = num;
+
+      if (solveSudokuHelper(board)) {
+        return board;
+      }
+
+      board[row][col] = 0;
+    }
+  }
+
+  return null;
+}
+
+function findEmptyCell(board) {
   for (let i = 0; i < 9; i++) {
     for (let j = 0; j < 9; j++) {
-      const input = document.createElement('input');
-      input.type = 'text';
-      input.className = 'cell';
-      input.maxLength = '1';
-      board.appendChild(input);
-      cells.push(input);
+      if (board[i][j] === 0) {
+        return [i, j];
+      }
+    }
+  }
+  return null;
+}
+
+function isValidMove(board, row, col, num) {
+  // Check row
+  for (let j = 0; j < 9; j++) {
+    if (board[row][j] === num) {
+      return false;
     }
   }
 
-  // Event listener for the solve button
-  solveBtn.addEventListener('click', () => {
-    const puzzle = [];
-    let index = 0;
-
-    // Create a 2D array representing the Sudoku puzzle
-    for (let i = 0; i < 9; i++) {
-      const row = [];
-      for (let j = 0; j < 9; j++) {
-        const value = parseInt(cells[index].value) || 0;
-        row.push(value);
-        index++;
-      }
-      puzzle.push(row);
+  // Check column
+  for (let i = 0; i < 9; i++) {
+    if (board[i][col] === num) {
+      return false;
     }
-
-    // Solve the puzzle
-    const solution = solveSudoku(puzzle);
-
-    // Update the board with the solved puzzle
-    solution.forEach((row, i) => {
-      row.forEach((value, j) => {
-        cells[i * 9 + j].value = value;
-      });
-    });
-  });
-
-  function solveSudoku(board) {
-    if (!findEmptyCell(board)) {
-      return board;
-    }
-
-    const [row, col] = findEmptyCell(board);
-
-    for (let num = 1; num <= 9; num++) {
-      if (isValidMove(board, row, col, num)) {
-        board[row][col] = num;
-
-        if (solveSudoku(board)) {
-          return board;
-        }
-
-        board[row][col] = 0;
-      }
-    }
-
-    return null;
   }
 
-  function findEmptyCell(board) {
-    for (let i = 0; i < 9; i++) {
-      for (let j = 0; j < 9; j++) {
-        if (board[i][j] === 0) {
-          return [i, j];
-        }
+  // Check 3x3 box
+  const startRow = Math.floor(row / 3) * 3;
+  const startCol = Math.floor(col / 3) * 3;
+  for (let i = startRow; i < startRow + 3; i++) {
+    for (let j = startCol; j < startCol + 3; j++) {
+      if (board[i][j] === num) {
+        return false;
       }
     }
-    return null;
   }
 
-  function isValidMove(board, row, col, num) {
-    // Check row
+  return true;
+}
+
+function animateSolution(solution) {
+  const delay = 100;
+
+  for (let i = 0; i < 9; i++) {
     for (let j = 0; j < 9; j++) {
-      if (board[row][j] === num) {
-        return false;
+      const cell = cells[i * 9 + j];
+      const value = solution[i][j];
+
+      if (value !== 0) {
+        setTimeout(() => {
+          cell.classList.add('fill-animation');
+          cell.value = value;
+        }, i * 9 * delay + j * delay);
       }
     }
-
-    // Check column
-    for (let i = 0; i < 9; i++) {
-      if (board[i][col] === num) {
-        return false;
-      }
-    }
-
-    // Check 3x3 box
-    const startRow = Math.floor(row / 3) * 3;
-    const startCol = Math.floor(col / 3) * 3;
-    for (let i = startRow; i < startRow + 3; i++) {
-      for (let j = startCol; j < startCol + 3; j++) {
-        if (board[i][j] === num) {
-          return false;
-        }
-      }
-    }
-
-    return true;
   }
+}
+
 function toggleMode() {
   const body = document.body;
   body.classList.toggle("dark-mode");
 
-  // Save the user's preference in local storage
   const isDarkMode = body.classList.contains("dark-mode");
   localStorage.setItem("darkMode", isDarkMode);
+
+  const primaryColorDelay = 3000; // 3 seconds delay
+  setTimeout(() => {
+    body.classList.add("hide-primary-color");
+  }, primaryColorDelay);
 }
 
-// Function to load the user's preference from local storage
 function loadModePreference() {
-  const isDarkMode = localStorage.getItem("darkMode");
-
-  if (isDarkMode === "true") {
-    document.body.classList.add("dark-mode");
+  const isDarkMode = localStorage.getItem("darkMode") === "true";
+  const body = document.body;
+  if (isDarkMode) {
+    body.classList.add("dark-mode");
   }
 }
+
+const switchInput = document.createElement("input");
+switchInput.type = "checkbox";
+switchInput.id = "switchInput";
+switchInput.checked = localStorage.getItem("darkMode") === "true";
+
+const slider = document.createElement("div");
+slider.className = "slider";
+slider.addEventListener("click", toggleMode);
+
+const switchElement = document.createElement("label");
+switchElement.className = "switch";
+switchElement.appendChild(switchInput);
+switchElement.appendChild(slider);
+
+document.body.appendChild(switchElement);
+
+loadModePreference();
